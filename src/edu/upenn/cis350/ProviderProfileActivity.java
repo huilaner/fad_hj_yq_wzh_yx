0817940c532a65ef61d93676a9631629afebe502
hui <huibiology@gmail.com>
2012-11-14 11:42:37 -0500

    solve my git problem

--- src/edu/upenn/cis350/ProviderProfileActivity.java
+++ src/edu/upenn/cis350/ProviderProfileActivity.java
@@ -618,46 +618,6 @@ public class ProviderProfileActivity extends Activity{
 		public long getItemId(int position) {
 			return position;
 		}
-		
-		
-		public String getProAndConString(int label) {
-			String message;
-			switch(label) {
-			case 1:
-			    message = getString(R.string.pros1);
-			    break;
-			case 2:
-			    message = getString(R.string.pros2);
-			    break;
-			case 3:
-			    message = getString(R.string.pros3);
-			    break;
-			case 4:
-			    message = getString(R.string.pros4);
-			    break;
-			case 5:
-			    message = getString(R.string.pros5);
-			    break;
-			case -1:
-			    message = getString(R.string.cons1);
-			    break;
-			case -2:
-			    message = getString(R.string.cons2);
-			    break;
-			case -3:
-			    message = getString(R.string.cons3);
-			    break;
-			case -4:
-			    message = getString(R.string.cons4);
-			    break;
-			case -5:
-			    message = getString(R.string.cons5);
-			    break;
-			default:
-			    message = "";
-			}
-			return message;
-		}
 
 
 		public String getProAndConString(int label) {
--- src/edu/upenn/cis350/entities/Rating.java
+++ src/edu/upenn/cis350/entities/Rating.java
@@ -23,13 +23,8 @@ public class Rating implements Serializable{
 	private Integer pro1 = 0;
 	private Integer pro2 = 0;
 	private Integer pro3 = 0;
-<<<<<<< HEAD
-	
-	
-=======
 
 
->>>>>>> add new cateogry page (sth wrong with my git, plz check if this version works for you)
 	public Rating(long user, long provider, String date, String review_summary, String desc, int rate,
 			int communication_rating, int office_environment_rating,
 			int friendliness_rating, int professional_rating, int costs_rating,
@@ -53,11 +48,7 @@ public class Rating implements Serializable{
 		this.con2 = con2;
 		this.con3 = con3;
 	}
-<<<<<<< HEAD
-	
-=======
 
->>>>>>> add new cateogry page (sth wrong with my git, plz check if this version works for you)
 	public Rating(long user, long provider, String review_summary, String date, String desc, int rate) {
 		this.user_id = user;
 		this.provider_id = provider;
@@ -110,11 +101,7 @@ public class Rating implements Serializable{
 	public String getReview_summary() {
 		return review_summary;
 	}
-<<<<<<< HEAD
-	
-=======
 
->>>>>>> add new cateogry page (sth wrong with my git, plz check if this version works for you)
 	public Integer getCon1() {
 		return con1;
 	}
@@ -162,8 +149,4 @@ public class Rating implements Serializable{
 	public void setPro3(Integer pro3) {
 		this.pro3 = pro3;
 	}
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> add new cateogry page (sth wrong with my git, plz check if this version works for you)
